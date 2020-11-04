2f6da9942d41ff5776e442365a2cf7b476ae0722
Nicola <nginvernizzi1@gmail.com>
2020-11-04 15:41:27 +0100

    html + css

--- js/main.js
+++ js/main.js
@@ -4,96 +4,97 @@ const icons = [
       name: 'cat',
       prefix: 'fa-',
       type: 'animal',
-      family: 'fas',
+      family: 'fas'
     },
     {
       name: 'crow',
       prefix: 'fa-',
       type: 'animal',
-      family: 'fas',
+      family: 'fas'
     },
     {
       name: 'dog',
       prefix: 'fa-',
       type: 'animal',
-      family: 'fas',
+      family: 'fas'
     },
     {
       name: 'dove',
       prefix: 'fa-',
       type: 'animal',
-      family: 'fas',
+      family: 'fas'
     },
     {
       name: 'dragon',
       prefix: 'fa-',
       type: 'animal',
-      family: 'fas',
+      family: 'fas'
     },
     {
       name: 'horse',
       prefix: 'fa-',
       type: 'animal',
-      family: 'fas',
+      family: 'fas'
     },
     {
       name: 'hippo',
       prefix: 'fa-',
       type: 'animal',
-      family: 'fas',
+      family: 'fas'
     },
     {
       name: 'fish',
       prefix: 'fa-',
       type: 'animal',
-      family: 'fas',
+      family: 'fas'
     },
     {
       name: 'carrot',
       prefix: 'fa-',
       type: 'vegetable',
-      family: 'fas',
+      family: 'fas'
     },
     {
       name: 'apple-alt',
       prefix: 'fa-',
       type: 'vegetable',
-      family: 'fas',
+      family: 'fas'
     },
     {
       name: 'lemon',
       prefix: 'fa-',
       type: 'vegetable',
-      family: 'fas',
+      family: 'fas'
     },
     {
       name: 'pepper-hot',
       prefix: 'fa-',
       type: 'vegetable',
-      family: 'fas',
+      family: 'fas'
     },
     {
       name: 'user-astronaut',
       prefix: 'fa-',
       type: 'user',
-      family: 'fas',
+      family: 'fas'
     },
     {
       name: 'user-graduate',
       prefix: 'fa-',
       type: 'user',
-      family: 'fas',
+      family: 'fas'
     },
     {
       name: 'user-ninja',
       prefix: 'fa-',
       type: 'user',
-      family: 'fas',
+      family: 'fas'
     },
     {
       name: 'user-secret',
       prefix: 'fa-',
       type: 'user',
-      family: 'fas',
-    },
+      family: 'fas'
+    }
 ];
+
